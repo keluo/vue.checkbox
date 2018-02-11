79c55b698281cbeb9da7f6fcfd1947342a3c6901
yw <yw@winasdaq.com>
2018-02-11 18:34:28 +0800

    @yw 第一次提交

---
+++ example/App.vue
@@ -0,0 +1,61 @@
+<template>
+<div>
+  <div class="block"  style="width: 80%;">
+    <span class="demonstration">基本点击</span>
+    <code>
+      
+    </code>
+    <div>
+      <vue-checkbox :value="checkboxValue" v-model="checkboxValue" label="选我"></vue-checkbox>
+    </div>
+  </div>
+  <div class="block"  style="width: 80%;">
+    <span class="demonstration">基本点击</span>
+    <code>
+      
+    </code>
+    <div>
+      <vue-checkbox  :value="checkboxValue" :disabled="checkboxDisabled" label="不可点击"></vue-checkbox>
+    </div>
+  </div>
+
+ <!--  <div>
+  <vue-checkbox :options="options" v-model="checkboxGroupValue"></vue-checkbox>
+  </div> -->
+</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      checkboxDisabled:true,
+      checkboxValue:true,
+      checkboxGroupValue:[],
+      options:[
+        {'label':'苹果','value':'1'},
+        {'label':'三星','value':'2'},
+        {'label':'小米','value':'3'},
+      ]
+    }
+  },
+  watch: {
+    checkboxValue (value) {
+      console.log(value);
+    },
+    checkboxGroupValue (value) {
+      console.log(value);
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style>
+.block {
+    padding: 30px 0;
+    -ms-flex: 1;
+    flex: 1;
+}
+</style>
---
+++ src/VueCheckbox.vue
@@ -0,0 +1,154 @@
+<template>
+  <!-- <label class="checkbox-wrap" v-if="options.length == 0"> -->
+  <label class="vue-checkbox-wrap" :class="disabled ? 'vue-checkbox-disabled' : ''">
+    <input type="checkbox" class="vue-checkbox-input" v-model="realValue" :disabled="disabled"/>
+    <span class="vue-checkbox-inner"></span>
+    <span class="vue-checkbox-txt" v-text="label" v-if="label"></span>
+  </label>
+  <!-- <div class="checkbox-group-wrap" v-else-if="options.length > 0">
+    <label class="checkbox-wrap" v-for="item in options">
+      <input type="checkbox" class="checkbox-input" :value="item.value" v-model="realValue"/>
+      <span class="checkbox-inner"></span>
+      <span class="checkbox-txt" v-text="item.label"></span>
+    </label>
+  </div> -->
+</template>
+
+<script>
+export default {
+  name: 'vue-checkbox',
+  props: {
+    // 'options':{
+    //   default:() => {
+    //     return [];
+    //   },
+    //   type:Array
+    // },
+    'label':{
+      default:() => {
+        return '';
+      },
+      type:String,
+    },
+    'disabled':{
+      default:() => {
+        return false;
+      },
+      type:Boolean,
+    },
+    'value':{
+      default:() => {
+        return false;
+      },
+      type:Boolean,
+    },
+  },
+  data () {
+    return {
+      //realValue: this.options.length > 0 ? [] : false
+      realValue: this.value
+    }
+  },
+  mounted () {
+  },
+  computed: {
+  },
+  watch: {
+    realValue (value) {
+      this.$emit('input', value);
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+ /*checkbox css new*/
+.vue-checkbox-group-wrap{
+  display: inline-block;
+}
+.vue-checkbox-group-wrap .checkbox-wrap{
+  margin-right: 10px;
+}
+.vue-checkbox-group-wrap .vue-checkbox-wrap:last-child{
+  margin-right: 0px;
+}
+.vue-checkbox-wrap{
+  white-space: nowrap;
+  display: inline-block;
+  position: relative;
+  cursor: pointer;
+  line-height: 1;
+}
+.vue-checkbox-wrap.vue-checkbox-disabled{
+  cursor: not-allowed;
+}
+.vue-checkbox-wrap .vue-checkbox-input{
+    position:absolute;
+    top:0px;
+    left:0px;
+}
+.vue-checkbox-wrap .vue-checkbox-txt {
+    padding-left: 5px;
+    float: left;
+}
+.vue-checkbox-wrap .vue-checkbox-inner{
+    position: relative;
+    top: 0;
+    left: 0;
+    display: block;
+    width: 14px;
+    height: 14px;
+    border: 1px solid #d9d9d9;
+    border-radius: 2px;
+    background-color: #fff;
+    -webkit-transition: all .3s;
+    transition: all .3s;
+    cursor: pointer;
+    float: left;
+}
+.vue-checkbox-wrap .vue-checkbox-inner:after{
+    -webkit-transform: rotate(45deg) scale(0);
+    -ms-transform: rotate(45deg) scale(0);
+    transform: rotate(45deg) scale(0);
+    position: absolute;
+    left: 4px;
+    top: 1px;
+    display: table;
+    width: 4px;
+    height: 8px;
+    border: 2px solid #fff;
+    border-top: 0;
+    border-left: 0;
+    content: " ";
+    -webkit-transition: all .2s cubic-bezier(.12,.4,.29,1.46) .1s;
+    transition: all .2s cubic-bezier(.12,.4,.29,1.46) .1s;
+    
+}
+.vue-checkbox-wrap.vue-checkbox-disabled .vue-checkbox-inner{  
+  border-color: #d9d9d9 !important;
+  background-color: #f5f5f5 !important;
+  cursor: not-allowed;
+}
+.vue-checkbox-wrap.vue-checkbox-disabled .vue-checkbox-inner:after{
+  -webkit-animation-name: none;
+  animation-name: none;
+  border-color: rgba(0, 0, 0, 0.25);
+}
+.vue-checkbox-wrap .vue-checkbox-input:checked + .vue-checkbox-inner{
+  background-color: #108ee9;
+  border-color: #108ee9;
+}
+.vue-checkbox-wrap .vue-checkbox-input:checked + .vue-checkbox-inner:after{
+  -webkit-transform: rotate(45deg) scale(1);
+  -ms-transform: rotate(45deg) scale(1);
+  transform: rotate(45deg) scale(1);
+}
+.vue-checkbox-wrap.vue-checkbox-disabled .vue-checkbox-txt{
+  color: rgba(0, 0, 0, 0.25);
+  cursor: not-allowed;
+}
+/*checkbox css new*/
+</style>
+
